796b4d98af7b9a96c12ac3d4d0aab6eec4d25d5b
Jet <junjet.tai@sony.com>
2024-09-26 17:47:02 +0900

    stash

--- src/main.py
+++ src/main.py
@@ -1,4 +1,48 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms
+
+DATA_CACHE = Path(__file__).parent.parent / "data/"
+MNIST_MEAN_VAR = (0.1307, 0.3081)
+
+
+def display_tensor_image(image: torch.Tensor) -> None:
+    """Displays an image tensor (from zero mean unit variance dataset)."""
+    # unnormalize the image
+    image = image * MNIST_MEAN_VAR[1] + MNIST_MEAN_VAR[0]
+
+    # display it
+    plt.imshow(image.cpu().numpy().swapaxes(0, -1))
+
+
+def get_dataset(train: bool) -> Dataset:
+    """Returns the MNIST dataset as a dataloader (so you can use iterators)."""
+    # define transformation to normalize image to 0 mean and unit variance
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((MNIST_MEAN_VAR[0],), (MNIST_MEAN_VAR[1],)),
+        ]
+    )
+
+    # return the dataloader
+    return datasets.MNIST(
+        root=DATA_CACHE,
+        train=True,
+        download=True,
+        transform=transform,
+    )
+
+
+def main() -> None:
+    for image, _ in get_dataset(train=True):
+        print(image.shape)
+        display_tensor_image(image)
+        exit()
 
 
 if __name__ == "__main__":
-    pass
+    main()
