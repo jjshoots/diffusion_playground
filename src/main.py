012e39c2d260335b0f46b5b802448625b76959de
Jet <taijunjet@hotmail.com>
2024-09-29 00:56:28 +0900

    stash

--- src/main.py
+++ src/main.py
@@ -1,11 +1,73 @@
+from pathlib import Path
+from typing import cast
+from tqdm import tqdm
+
+import torch
+import torch.optim as opt
+from torch.utils.data import DataLoader
+from wingman import Wingman
+
+from model import DiffusionModel
 from utils import display_tensor_image, get_dataset
 
 
 def main() -> None:
-    for image, _ in get_dataset(train=True):
-        print(image.shape)
-        display_tensor_image(image)
-        exit()
+    wm = Wingman(Path(__file__).parent.parent / "config.yaml")
+
+    # load the dataset
+    trainloader = DataLoader(
+        get_dataset(train=True),
+        batch_size=wm.cfg.batch_size,
+        shuffle=True,
+        pin_memory=True,
+    )
+
+    # load the model, optimizer, loss_fn
+    model = DiffusionModel(
+        sampling_steps=wm.cfg.sampling_steps,
+    ).to(wm.device)
+    optim = opt.AdamW(
+        model.parameters(),
+        lr=wm.cfg.learning_rate,
+    )
+    loss_fn = torch.nn.MSELoss()
+
+    # compile
+    model = torch.compile(model)
+    model = cast(DiffusionModel, model)
+
+    # begin training
+    for epoch in range(wm.cfg.epochs):
+        for images, labels in tqdm(trainloader):
+            images = images.to(wm.device)
+            labels = labels.to(wm.device)
+
+            # sample timesteps
+            ts = torch.randint(
+                low=1,
+                high=wm.cfg.sampling_steps + 1,
+                size=(len(images), 1, 1, 1),
+                device=wm.device,
+            )
+
+            # noise the images according to a schedule
+            noised_images, noises = model.forward_diffusion(images, ts)
+
+            # perform reverse diffusion
+            predicted_noises = model.predict_noise(noised_images, ts)
+
+            # loss function, zero grad, backward, step
+            loss = loss_fn(predicted_noises, noises)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            # record and checkpoint
+            to_save, model_dir, mark_dir = wm.checkpoint(
+                loss=loss.detach().cpu().numpy(),
+                step=epoch
+            )
+            torch.save(model.state_dict(), mark_dir / "weights.pth")
 
 
 if __name__ == "__main__":
--- src/model.py
+++ src/model.py
@@ -5,37 +5,42 @@ from torch import nn
 class DiffusionModel(nn.Module):
     """DiffusionModel."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        sampling_steps: int
+    ) -> None:
         """__init__."""
         super().__init__()
 
-        self.model = nn.ModuleList(
-            [
-                nn.Conv2d(
-                    in_channels=1,
-                    out_channels=16,
-                    kernel_size=5,
-                    padding=2,
-                ),
-                nn.Conv2d(
-                    in_channels=16,
-                    out_channels=32,
-                    kernel_size=5,
-                    padding=2,
-                ),
-                nn.Conv2d(
-                    in_channels=32,
-                    out_channels=16,
-                    kernel_size=5,
-                    padding=2,
-                ),
-                nn.Conv2d(
-                    in_channels=16,
-                    out_channels=2,
-                    kernel_size=5,
-                    padding=2,
-                ),
-            ],
+        # some constants
+        self.sampling_steps = sampling_steps
+
+        # define the model
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(
+                in_channels=2,
+                out_channels=16,
+                kernel_size=5,
+                padding=2,
+            ),
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=5,
+                padding=2,
+            ),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=5,
+                padding=2,
+            ),
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=2,
+                kernel_size=5,
+                padding=2,
+            ),
         )
 
     def forward(
@@ -53,16 +58,31 @@ class DiffusionModel(nn.Module):
             torch.Tensor: mean of predicted noise.
             torch.Tensor: var of predicted noise.
         """
+        # append the timestep embedding
+        x = torch.concat(
+            (
+                x,
+                (torch.ones_like(x) * t) - (self.sampling_steps / 2.0),
+            ),
+            dim=-3,
+        )
+
         # pass input through model
-        for f in self.model:
-            x = x + f(x)
+        y = self.conv_layers(x)
 
         # split into mean, var
-        noise_mean, noise_var = torch.split(x, 1, dim=-3)
-        return noise_mean, noise_var
+        noise_mean, noise_log_var = torch.split(y, 1, dim=-3)
+        return noise_mean, noise_log_var
 
-    @staticmethod
-    def get_alpha(t: torch.Tensor) -> torch.Tensor:
+    def predict_noise(
+        self,
+        x: torch.Tensor,
+        t: torch.Tensor,
+    ) -> torch.Tensor:
+        mean, log_var = self(x, t)
+        return torch.distributions.Normal(mean, torch.exp(log_var)).rsample()
+
+    def get_alpha(self, t: torch.Tensor) -> torch.Tensor:
         """get_alpha.
 
         Args:
@@ -71,10 +91,9 @@ class DiffusionModel(nn.Module):
         Returns:
             torch.Tensor: [...] alpha in [1, 0].
         """
-        return DiffusionModel.get_cum_alpha(t) / DiffusionModel.get_cum_alpha(t - 1)
+        return self.get_cum_alpha(t) / self.get_cum_alpha(t - 1)
 
-    @staticmethod
-    def get_cum_alpha(t: torch.Tensor) -> torch.Tensor:
+    def get_cum_alpha(self, t: torch.Tensor) -> torch.Tensor:
         """get_alpha.
 
         Args:
@@ -83,11 +102,11 @@ class DiffusionModel(nn.Module):
         Returns:
             torch.Tensor: [...] alpha in [1, 0].
         """
-        t = t.clamp(min=1e-2, max=1.0)
-        return torch.cos(torch.pi / 2.0 * t) ** 2
+        t = t.clamp(min=0.0, max=self.sampling_steps)
+        return torch.cos(torch.pi / 2.0 * t / self.sampling_steps) ** 2
 
-    @staticmethod
     def forward_diffusion(
+        self,
         x: torch.Tensor,
         t: torch.Tensor,
     ) -> tuple[torch.Tensor, torch.Tensor]:
@@ -103,7 +122,7 @@ class DiffusionModel(nn.Module):
         """
         # sample an absolute noise and an alpha
         noise = torch.randn_like(x)
-        alpha = DiffusionModel.get_alpha(t)
+        alpha = self.get_alpha(t)
 
         # noise the image
         noised_x = torch.sqrt(alpha) * x + torch.sqrt(1 - alpha) * noise
@@ -115,19 +134,28 @@ class DiffusionModel(nn.Module):
         x: torch.Tensor,
         t: torch.Tensor,
     ) -> torch.Tensor:
+        """reverse_diffusion.
+
+        Args:
+            x (torch.Tensor): [..., C, H, W] image.
+            t (torch.Tensor): [..., 1] noise schedule in [0, 1].
+
+        Returns:
+            torch.Tensor: the input image minus the predicted noise at the timestep
+        """
         # get alphas
         alpha = self.get_alpha(t)
         cum_alpha = self.get_cum_alpha(t)
 
         # predict noise
-        pred_noise_mean, pred_noise_var = self(x, t)
+        pred_noise_mean, pred_noise_log_var = self(x, t)
 
         # reverse the process
         # fmt: off
         clean_x = (
             (1 / torch.sqrt(alpha))
             * (x - ((1 - alpha) / torch.sqrt(1 - cum_alpha)) * pred_noise_mean)
-            + torch.randn_like(x) * pred_noise_var
+            + torch.randn_like(x) * torch.exp(pred_noise_log_var)
         )
         # fmt: on
 
